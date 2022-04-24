c14ed685f9ad4b65ce656b51f6bbdc024e5e98ce
093309 <markushka0909@gmail.com>
2022-04-24 21:04:04 +0300

    fixed json

--- src/components/home/services/ServicesData.js
+++ src/components/home/services/ServicesData.js
@@ -1,39 +1,45 @@
+import services1 from '../../../img/services/services-1.jpg';
+import services2 from '../../../img/services/services-2.jpg';
+import services3 from '../../../img/services/services-3.jpg';
+import services4 from '../../../img/services/services-4.jpg';
+import services5 from '../../../img/services/services-5.jpg';
+
 const ServicesData = [
   {
     id: 1,
     title: "Web Design",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-1.jpg",
+    cover: services1,
   },
   {
     id: 2,
     title: "Web Development",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-2.jpg",
+    cover: services2,
   },
   {
     id: 3,
     title: "Photography",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-3.jpg",
+    cover: services1,
   },
   {
     id: 4,
     title: "Live Support",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-4.jpg",
+    cover: services3,
   },
   {
     id: 5,
     title: "Content Writing",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-5.jpg",
+    cover: services4,
   },
   {
     id: 6,
     title: "Timely Deliverables",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-6.jpg",
+    cover: services5,
   },
 ]
 
--- src/components/home/work/Data.js
+++ src/components/home/work/Data.js
@@ -1,39 +1,45 @@
+import work1 from '../../../img/work/work1.jpg';
+import work2 from '../../../img/work/work2.jpg';
+import work3 from '../../../img/work/work3.jpg';
+import work4 from '../../../img/work/work4.jpg';
+import work5 from '../../../img/work/work5.jpg';
+import work6 from '../../../img/work/work6.jpg';
 const Data = [
   {
     id: 1,
     title: "Digital Marketing",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    cover: "./assets/work/work1.jpg",
+    cover: work1,
   },
   {
     id: 2,
     title: "Digital Marketing",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    cover: "./assets/work/work2.jpg",
+    cover: work2,
   },
   {
     id: 3,
     title: "Digital Marketing",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    cover: "./assets/work/work3.jpg",
+    cover: work3,
   },
   {
     id: 4,
     title: "Digital Marketing",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    cover: "./assets/work/work4.jpg",
+    cover: work4,
   },
   {
     id: 5,
     title: "Digital Marketing",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    cover: "./assets/work/work5.jpg",
+    cover: work5,
   },
   {
     id: 6,
     title: "Digital Marketing",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    cover: "./assets/work/work6.jpg",
+    cover: work6,
   },
 ]
 
